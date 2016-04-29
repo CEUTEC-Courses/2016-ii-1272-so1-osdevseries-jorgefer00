583d376cc957834e174233a868ebea8fb4531fd9
Jorge Fernandez <jorge.fernandez@kapa7.com>
2016-04-28 20:39:27 -0500

    Script creado para correr tutoriales. Sera necesario tener el paquete dialog instalado.

---
+++ CorrerTutoriales.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while true; do
+dialog --title "OSDev Jorge Fernandez" \
+--cancel-label "Salir" \
+--menu "Por favor seleccione un tutorial a correr en BOCHS" 15 55 3 \
+1 "Tutorial 3" \
+2 "Tutorial 4" \
+3 "Tutorial 6" 2>temporal
+
+if [ "$?" = "0" ]
+then
+	seleccion=$(cat temporal)
+	rm -rf temporal
+	if [ $seleccion = "1" ]
+	then
+		glxgears >2
+	fi
+	if [ $seleccion = "2" ]
+	then
+		echo "Correr Tutorial 4"
+	fi
+	if [ $seleccion = "3" ]
+	then
+		echo "Correr Tutorial 6"
+	fi
+else
+	rm -rf temporal
+	clear
+	break
+fi
+done
