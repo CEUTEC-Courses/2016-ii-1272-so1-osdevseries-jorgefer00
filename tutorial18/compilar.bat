fc8f13a817fbb08f6ee00b5fde9b63df88054ce9
jorgefer00 <jorge.fernandez@kapa7.com>
2016-05-11 20:41:33 -0600

    Terminado codigo hasta tutorial 19. solo faltan los docs.

---
+++ tutorial18/compilar.bat
@@ -0,0 +1,9 @@
+@echo off
+nasm -f bin boot1.asm -o boot.bin
+nasm -f bin boot2.asm -o KRNLDR.SYS
+dd if=boot.bin of=floppy.img bs=512 count=1 conv=notrunc seek=0
+vfd.exe OPEN A: floppy.img /W /1.44
+copy KRNLDR.SYS A:
+copy "SysCore\debug\Kernel.exe" "A:\KRNL32.EXE"
+vfd.exe CLOSE A: 
+pause
